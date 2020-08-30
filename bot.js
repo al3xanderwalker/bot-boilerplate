beef71a54ab8519bd7d2300b653195e9461e176c
al3xanderwalker <48996881+al3xanderwalker@users.noreply.github.com>
2020-08-30 15:26:39 +0100

    Initial Commit Boilerplate

---
+++ bot.js
@@ -0,0 +1,25 @@
+const requireAll = require('require-all'),
+  Discord = require('discord.js'),
+  client = new Discord.Client(),
+  config = require('./config.json');
+
+const events = requireAll({
+  dirname: `${__dirname}/events`,
+  filter: /^(?!-)(.+)\.js$/,
+});
+client.removeAllListeners();
+for (const name in events) {
+  const event = events[name];
+  client.on(name, event.bind(null, client));
+}
+const commands = requireAll({
+  dirname: `${__dirname}/commands`,
+  filter: /^(?!-)(.+)\.js$/,
+});
+client.commands = new Map();
+for (const name in commands) {
+  const cmd = commands[name];
+  client.commands.set(cmd.config.name, cmd);
+  console.log(`Command loaded: ${cmd.config.name}`);
+}
+client.login(config.token);
